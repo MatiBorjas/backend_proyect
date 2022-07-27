be9396b5b066b73f27df81066fa8ebf91e265b73
Matias Borjas <matias.borjas@gmail.com>
2022-07-27 10:30:05 -0300

    Desafio clases #5 y #6

--- index.js
+++ index.js
@@ -1,137 +1,82 @@
-const fs = require('fs');
-const { parse } = require('path');
+const fs = require('fs')
+
+//---------------------------
+//        CLASE
+//---------------------------
 
 class Contenedor {
 
-    constructor(archivo) {
-        this.archivo = "./archivos/"+archivo+".json"
-      };    
-
-//---------------------------
-//        METODO SAVE
-//---------------------------
-    async save(obj){
-      try{
-        const datos = await fs.promises.readFile(this.archivo,'utf-8')
-        let datosJson = JSON.parse(datos);
-        let entrenamientos = [];
-        const indice = datosJson.map(elem => elem.id).sort();
-        obj.id = indice[indice.length - 1] + 1 
-
-        if(!obj.id){
-          obj.id = 1;
-          entrenamientos = [{...obj}];
-          await fs.promises.writeFile(this.archivo, JSON.stringify(entrenamientos));
-          return entrenamientos[0].id
-        }
-
-        datosJson.push(obj)
-
-        await fs.promises.writeFile(this.archivo, JSON.stringify(datosJson))
-        console.log(`El elemento: \n${JSON.stringify(obj)} \nfue guarado exitosamente`)
-      }
-      catch(error){
-        console.log('ERROR! ' + error)
-      }
-
+  constructor(archivo) {
+      this.archivo = "./archivos/"+archivo+".json"
     };
 
-//---------------------------
-//METODO DELETE BY ID
-//---------------------------
-
-  async getById(id){
-    try {
-      const datos = await fs.promises.readFile(this.archivo,'utf-8')
-      let datosJson = JSON.parse(datos);
-      
-      if(datosJson.find(elemento => elemento.id === id)){
-        console.log(datosJson.find(elemento => elemento.id === id))
-      } else {
-        console.log(`El elemento con id numero ${id} no existe`)
-      }
-    }
-    catch (error) {
-      console.log('La busqueda no pudo realizarse correctamente' + error);
-    }
-  };
-
-//---------------------------
-//    METODO GETALL
-//---------------------------
+  //---------------------------
+  //    METODO GET ALL
+  //---------------------------
   async getAll(){
     try {
       const datos = await fs.promises.readFile(this.archivo,'utf-8');
-      return console.log(JSON.parse(datos));      
+      let datosParse = JSON.parse(datos);
+      return datosParse;
     } 
     catch (error) {
       console.log('Hubo un error en la lectura de archivos')
     }
-    
   };
 
-//---------------------------
-//METODO DELETE BY ID
-//---------------------------
-
-  async deleteById(id){
-    try {
-      const datos = await fs.promises.readFile(this.archivo,'utf-8');
-      let datosJson = JSON.parse(datos);
-
-      if(datosJson.some(elemento => elemento.id === id)){
-        let objIndex = datosJson.indexOf(datosJson.find(elemento => elemento.id === id));
-        datosJson.splice(objIndex, objIndex);
-        await fs.promises.writeFile(this.archivo, JSON.stringify(datosJson));
-        console.log(`El elemento con id: ${id} fue eliminado con exito`)
-
-      } else {
-        console.log(`No existe un elemento con el id ${id}`)
-      }
-    } 
-    catch (error) {
-      
-    }
-  };
 
   //---------------------------
-//METODO DELETE ALL
-//---------------------------
-
-  async deleteAll(){
-    try {
-      await fs.promises.writeFile(this.archivo, '[]');
-      console.log('Todos los elementos del archivo han sido eliminados')
-    } catch (error) {
-      console.log(error)
-    }
-  };
+  //  METODO PRODUCTO RANDOM
+  //---------------------------
+async productoRandom(){
+  try {
+    const datos = await fs.promises.readFile(this.archivo,'utf-8');
+    let datosParse = JSON.parse(datos);
+    let randomIndex = Math.floor(Math.random()*datosParse.length);
+    let productoRandom = datosParse[randomIndex];
+    
+    return productoRandom;
+  } 
+  catch (error) {
+    console.log('Hubo un error en la lectura de archivos')
+  }
+};
 }
 
 
-// ---------------------------------------------------------------------
 
+const productos = new Contenedor('datosEntrenamientos');
 
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 3000;
 
-const objetoInicial1 = {
-  nombre: 'Matias',
-  apellido: 'Borjas'
-}
+const server = app.listen(PORT, () => {
+  console.log(`Servidor http escuchando en el puerto ${server.address().port}`);
+});
 
-const objetoInicial2 = {
-  nombre: 'Zoila',
-  apellido: 'Cerda'
-}
+server.on("error", (error) => console.log(`Error en servidor ${error}`));
 
-const objetoInicial3 = {
-  nombre: 'Elba',
-  apellido: 'Garto'
-}
+app.get("/", (req, res) => {
+  res.send({ mensaje: "Bienvenidos a Hipercompumundo mega red" });
+});
 
-const objeto1 = new Contenedor('datosEntrenamientos');
-// objeto1.traerDatos();
-// objeto1.save(objetoInicial3);
-// objeto1.getAll();
-// objeto1.getById(3);
-// objeto1.deleteById(3);
-// objeto1.deleteAll();
+app.get("/productos", async(req, res) => {
+
+  try {
+    let products = await productos.getAll()
+    res.send(products);
+  } catch (error) {
+    console.log(error)
+  }
+
+});
+
+app.get("/productoRandom", async(req, res) => {
+  try {
+    let products = await productos.productoRandom()
+    res.send(products);
+  } catch (error) {
+    console.log(error)
+  }
+});
---
+++ index_desafio_clases_#3_#4.js
@@ -0,0 +1,138 @@
+const fs = require('fs');
+const { parse } = require('path');
+const { mainModule } = require('process');
+
+class Contenedor {
+
+    constructor(archivo) {
+        this.archivo = "./archivos/"+archivo+".json"
+      };
+
+//---------------------------
+//        METODO SAVE
+//---------------------------
+    async save(obj){
+      try{
+        const datos = await fs.promises.readFile(this.archivo,'utf-8')
+        let datosJson = JSON.parse(datos);
+        let entrenamientos = [];
+        const indice = datosJson.map(elem => elem.id).sort();
+        obj.id = indice[indice.length - 1] + 1 
+
+        if(!obj.id){
+          obj.id = 1;
+          entrenamientos = [{...obj}];
+          await fs.promises.writeFile(this.archivo, JSON.stringify(entrenamientos));
+          return entrenamientos[0].id
+        }
+
+        datosJson.push(obj)
+
+        await fs.promises.writeFile(this.archivo, JSON.stringify(datosJson))
+        console.log(`El elemento: \n${JSON.stringify(obj)} \nfue guarado exitosamente`)
+      }
+      catch(error){
+        console.log('ERROR! ' + error)
+      }
+
+    };
+
+//---------------------------
+//METODO DELETE BY ID
+//---------------------------
+
+  async getById(id){
+    try {
+      const datos = await fs.promises.readFile(this.archivo,'utf-8')
+      let datosJson = JSON.parse(datos);
+      
+      if(datosJson.find(elemento => elemento.id === id)){
+        console.log(datosJson.find(elemento => elemento.id === id))
+      } else {
+        console.log(`El elemento con id numero ${id} no existe`)
+      }
+    }
+    catch (error) {
+      console.log('La busqueda no pudo realizarse correctamente' + error);
+    }
+  };
+
+//---------------------------
+//    METODO GETALL
+//---------------------------
+  async getAll(){
+    try {
+      const datos = await fs.promises.readFile(this.archivo,'utf-8');
+      return console.log(JSON.parse(datos));      
+    } 
+    catch (error) {
+      console.log('Hubo un error en la lectura de archivos')
+    }
+    
+  };
+
+//---------------------------
+//METODO DELETE BY ID
+//---------------------------
+
+  async deleteById(id){
+    try {
+      const datos = await fs.promises.readFile(this.archivo,'utf-8');
+      let datosJson = JSON.parse(datos);
+
+      if(datosJson.some(elemento => elemento.id === id)){
+        let objIndex = datosJson.indexOf(datosJson.find(elemento => elemento.id === id));
+        datosJson.splice(objIndex, objIndex);
+        await fs.promises.writeFile(this.archivo, JSON.stringify(datosJson));
+        console.log(`El elemento con id: ${id} fue eliminado con exito`)
+
+      } else {
+        console.log(`No existe un elemento con el id ${id}`)
+      }
+    } 
+    catch (error) {
+      
+    }
+  };
+
+  //---------------------------
+//METODO DELETE ALL
+//---------------------------
+
+  async deleteAll(){
+    try {
+      await fs.promises.writeFile(this.archivo, '[]');
+      console.log('Todos los elementos del archivo han sido eliminados')
+    } catch (error) {
+      console.log(error)
+    }
+  };
+}
+
+
+// ---------------------------------------------------------------------
+
+
+
+const objetoInicial1 = {
+  nombre: 'Matias',
+  apellido: 'Borjas'
+}
+
+const objetoInicial2 = {
+  nombre: 'Zoila',
+  apellido: 'Cerda'
+}
+
+const objetoInicial3 = {
+  nombre: 'Elba',
+  apellido: 'Garto'
+}
+
+const objeto1 = new Contenedor('datosEntrenamientos');
+// objeto1.traerDatos();
+// objeto1.save(objetoInicial3);
+objeto1.getAll()
+// objeto1.getById(3);
+// objeto1.deleteById(3);
+// objeto1.deleteAll();
